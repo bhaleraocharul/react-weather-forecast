ec859ce1296fd4b8c46ef1aded1e18496868f963
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-04-23 19:20:58 +1000

    Added the github repo url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App </h1>
+      <footer>
+        This project was created by{" "}
+        <a
+          href="https://charul-bhalerao.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Charul
+        </a>{" "}
+        and is open sourced on{" "}
+        <a
+          href="https://github.com/bhaleraocharul/react-weather-forecast"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </footer>
     </div>
   );
 }
