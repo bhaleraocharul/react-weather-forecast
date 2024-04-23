4980e6f60e0bd9bf783a89f2e50c6197282e822c
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-04-23 19:59:09 +1000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
-      <footer>
-        This project was created by{" "}
-        <a
-          href="https://charul-bhalerao.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Charul
-        </a>{" "}
-        and is open sourced on{" "}
-        <a
-          href="https://github.com/bhaleraocharul/react-weather-forecast"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App </h1>
+        <footer>
+          This project was created by{" "}
+          <a
+            href="https://charul-bhalerao.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Charul
+          </a>{" "}
+          and is open sourced on{" "}
+          <a
+            href="https://github.com/bhaleraocharul/react-weather-forecast"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
