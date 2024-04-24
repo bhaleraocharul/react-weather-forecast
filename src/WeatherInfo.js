a1c1a9baf5febc16f4b37e6280e98523606d0167
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-04-24 17:56:18 +1000

    added the npn react animated weather

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,11 +15,13 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <img
-              src={props.data.iconUrl}
-              alt="Weather data description"
-              className="float-left"
-            />
+            <div className="float-left">
+              <WeatherIcon
+                code={props.data.icon}
+              
+              />
+            </div>
+  
             <div className="float-left">
               <span className="temperature">{props.data.temperature}</span>
               <span className="unit">°C</span>
