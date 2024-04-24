c464d65a1d9e5e798cf99faee31bb58de0d2e326
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-04-24 17:21:43 +1000

    Added the icon url

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       city: response.data.name,
       humidity: response.data.main.humidity,
-      iconUrl: "",
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       date: new Date(response.data.dt * 1000),
     });
   }
