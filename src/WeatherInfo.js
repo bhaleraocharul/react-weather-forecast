7cfa5d0dfdf29e7ab662c78494a9c658fafa6eb4
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-04-24 18:28:46 +1000

    added the conversion from C to F

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -16,15 +17,11 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
-              <WeatherIcon
-                code={props.data.icon}
-              
-              />
+              <WeatherIcon code={props.data.icon} />
             </div>
-  
+
             <div className="float-left">
-              <span className="temperature">{props.data.temperature}</span>
-              <span className="unit">°C</span>
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
