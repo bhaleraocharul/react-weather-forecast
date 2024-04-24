e0687c25207976e9e34fa1bf0bb4d28c62a38e4f
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-04-24 21:11:32 +1000

    css changes

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,19 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
+import axios from "axios";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "88724523008dc9e1be18f6eb6a959b67";
+  let longitude = 40.7;
+  let latitude = 74;
+  //let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  //axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
