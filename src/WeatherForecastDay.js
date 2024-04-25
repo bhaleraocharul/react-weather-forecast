06520c8fbc745907c76a87eb86174f9064dd5501
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-04-25 14:17:56 +1000

    Update WeatherForecastDay.js

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,19 +2,28 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
-    
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+
   return (
     <div className="WeatherForecastDay">
       <div className="WeatherForecast-day">{props.data.dt}</div>
       <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
-          {props.data.temp.max}°
+          {maxTemperature}
         </span>
         <span className="WeatherForecast-temperature-min">
-          {props.data.temp.min}°
+          {minTemperature}
         </span>
       </div>
     </div>
-  ); 
+  );
 }
